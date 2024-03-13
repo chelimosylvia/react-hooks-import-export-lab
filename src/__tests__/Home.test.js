634d154547ca8fcf4c52f67a1e1d8a490698dd45
Sylviachelimo <chelimosylvia280@gmail.com>
2024-03-13 11:10:49 +0300

    Completed Assignment

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,11 +1,11 @@
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import Home from "../components/Home";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../components/Home';  // Adjust the import path based on your project structure
 
-test("it is exported as a default export", () => {
+test('it is exported as a default export', () => {
   try {
     expect(() => render(<Home />)).not.toThrow();
   } catch (e) {
-    throw new Error("Make sure to export this component!");
+    throw new Error('Make sure to export this component!');
   }
 });
